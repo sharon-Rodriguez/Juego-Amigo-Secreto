379085ce0e31036f8e4ce8c33a266864493ca2bd
Sharon Rodriguez <sharo2599@gmail.com>
2025-08-18 10:11:24 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 let amigos = [];
+
 function agregarAmigo(){
     const amigoInput = document.getElementById('amigo').value;
+
     if(amigoInput.trim() === ''){
         alert("Por favor, escribe un nombre");
         return;
     } else {
         amigos.push(amigoInput);
     }
-
     document.getElementById('amigo').value = '';
+}
+
+function actualizarLista(){ 
+
+const ul = document.querySelector('#listaAmigos');
+ul.innerHTML = '';
+
+for (const nombre of amigos){
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    ul.appendChild(li);
+}
 }
