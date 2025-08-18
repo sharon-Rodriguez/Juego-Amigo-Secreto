5b66b19abaa7bc19177b48f1f25222301083cc73
Sharon Rodriguez <sharo2599@gmail.com>
2025-08-18 01:11:03 -0500

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let amigos = [];
+function agregarAmigo(){
+    const amigoInput = document.getElementById('amigo').value;
+    if(amigoInput.trim() === ''){
+        alert("Por favor, escribe un nombre");
+        return;
+    } else {
+        amigos.push(amigoInput);
+    }
+
+    document.getElementById('amigo').value = '';
+}
